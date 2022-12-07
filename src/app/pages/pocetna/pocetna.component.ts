84bbea9f915be690bc6f2f3ec91c9546ad0e3063
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-07 20:42:04 +0100

    IT255-PZ

---
+++ src/app/pages/pocetna/pocetna.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pocetna',
+  templateUrl: './pocetna.component.html',
+
+})
+export class PocetnaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
