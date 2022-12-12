9b212ff88374511d1451a7b154aeb8d2f93fc391
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2022-12-12 15:55:32 +0100

    IT255-PZ

--- src/app/pages/pocetna/pocetna.component.ts
+++ src/app/pages/pocetna/pocetna.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pocetna',
   templateUrl: './pocetna.component.html',
-
 })
 export class PocetnaComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  imageObject: Array<object> = [
+    {
+      image: 'assets/img/spolja1.jpg',
+      thumbImage: 'assets/img/spolja1.jpg',
+      alt: 'alt of image',
+      title: 'Planet Bojana Las Vegas',
+    },
+    {
+      image: 'assets/img/spolja2.jpg',
+      thumbImage: 'assets/img/spolja2.jpg',
+      title: 'Planet Bojana Coachella Valley',
+      alt: 'Image alt',
+      order: 1,
+    },
+    {
+      image: 'assets/img/unutra1.jpg',
+      thumbImage: 'assets/img/unutra1.jpg',
+      title: 'Planet Bojana Cancun',
+      alt: 'Image altt',
+    },
+    {
+      image: 'assets/img/unutra2.jpg',
+      thumbImage: 'assets/img/unutra2.jpg',
+      title: 'Planet Bojana Paris',
+      alt: 'Image alternative',
+    },
+    {
+      image: 'assets/img/unutra3.jpg',
+      thumbImage: 'assets/img/unutra3.jpg',
+      title: 'Planet Bojana New York',
+      alt: 'Image alte',
+    },{
+      image: 'assets/img/unutra4.jpg',
+      thumbImage: 'assets/img/unutra4.jpg',
+      title: 'Planet Bojana Sydney',
+      alt: 'Image alter',
+    }
+  ];
 }
